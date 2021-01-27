5d9d5232053a9e74c66b4b7e211124cac5966282
lihuan <767623951@qq.com>
2021-01-27 15:59:49 +0800

    idea first  1

--- HelloGit/src/main/java/com/lihuan/git/HelloGit.java
+++ HelloGit/src/main/java/com/lihuan/git/HelloGit.java
@@ -5,6 +5,9 @@ package com.lihuan.git;
  */
 public class HelloGit {
     public static void main(String[] args) {
+
         System.out.println("Hello Git!");
+        System.out.println("更新1");
+
     }
 }
