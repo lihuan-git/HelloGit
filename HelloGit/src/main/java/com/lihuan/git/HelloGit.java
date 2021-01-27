94d0cf874d8341aca77e98509d0f6d044367969f
lihuan <767623951@qq.com>
2021-01-27 15:57:08 +0800

    idea first

---
+++ HelloGit/src/main/java/com/lihuan/git/HelloGit.java
@@ -0,0 +1,10 @@
+package com.lihuan.git;
+
+/**
+ * Created by lihuan on 2021/1/27.
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
