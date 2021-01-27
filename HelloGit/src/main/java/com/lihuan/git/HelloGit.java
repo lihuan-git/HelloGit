b09b0e15569f87a380f1eccdf2baa32ced26c7fc
lihuan-git <78065408+lihuan-git@users.noreply.github.com>
2021-01-27 17:40:57 +0800

    在线更新

--- HelloGit/src/main/java/com/lihuan/git/HelloGit.java
+++ HelloGit/src/main/java/com/lihuan/git/HelloGit.java
@@ -14,5 +14,6 @@ public class HelloGit {
         System.out.println("分枝1");
         System.out.println("主干添加1");
         System.out.println("分枝添加");
+        System.out.println("在线更新");
     }
 }
